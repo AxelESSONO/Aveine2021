e30808f74cdaae4ef9f436dcf5ce77e49f1e05d7
Axel ESSONO <naxelcdric@gmail>
2021-02-15 15:09:04 +0100

    Designed Splash Screen and created HomeActivity.kt

---
+++ app/src/main/java/com/aveine/aveine2021/view/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.aveine.aveine2021.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aveine.aveine2021.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/aveine/aveine2021/view/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/aveine/aveine2021/view/activity/SplashScreenActivity.kt
@@ -1,12 +1,48 @@
 package com.aveine.aveine2021.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.aveine.aveine2021.R
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    // Variables
+    lateinit var topAnimation: Animation
+    lateinit var bottomAnimation: Animation
+    lateinit var logo_image: ImageView
+    private val SPLASH_SCREEN: Long = 5000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
         setContentView(R.layout.activity_splash_screen)
+
+        //Animations
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        // Hooks
+        logo_image = findViewById<ImageView>(R.id.aveine_logo)
+
+        // set Animation
+        logo_image.animation = topAnimation
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreenActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_SCREEN)
+
     }
 }
