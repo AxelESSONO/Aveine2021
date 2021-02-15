fc7afcc57dc1383e938f0a3caff7a62d022de8cb
Axel ESSONO <naxelcdric@gmail>
2021-02-15 16:08:42 +0100

    Implemented HomeActivity.kt

--- app/src/main/java/com/aveine/aveine2021/view/activity/HomeActivity.kt
+++ app/src/main/java/com/aveine/aveine2021/view/activity/HomeActivity.kt
@@ -2,11 +2,34 @@ package com.aveine.aveine2021.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import com.aveine.aveine2021.R
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
     }
+
+    // Link menu to MainActivity
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Load menu described in menu_toolbar.xml file
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    // Task to perform by selected menu item
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return true
+    }
+
 }
