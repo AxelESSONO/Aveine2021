b89e769a22b3ec6dc25aa4dab8001b1dbba1df18
Axel ESSONO <naxelcdric@gmail>
2021-02-15 14:45:01 +0100

    Initial commit

---
+++ app/src/main/java/com/aveine/aveine2021/view/activity/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.aveine.aveine2021.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aveine.aveine2021.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
